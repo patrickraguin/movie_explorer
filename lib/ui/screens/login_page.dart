c42f9990fe8dee831d35c04465de8ecb48126d84
patrickraguin <patrick.raguin@gmail.com>
2023-04-25 22:42:55 +0200

    Commentaire

--- lib/ui/screens/login_page.dart
+++ lib/ui/screens/login_page.dart
@@ -1,31 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movie_explorer/blocs/user_cubit.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Clé permettant d'accéder au widget Form
+    final GlobalKey<FormState> formKey = GlobalKey();
+
+    // Controleur permettant d'accéder à la valeur du champs de saisie
     final TextEditingController usernameController = TextEditingController();
     final TextEditingController passwordController = TextEditingController();
 
     return SafeArea(
         child: Scaffold(
-      body: Column(
-        children: [
-          TextFormField(
-            controller: usernameController,
-          ),
-          TextFormField(
-            controller: passwordController,
-          ),
-          ElevatedButton(
-              onPressed: () async {
-                await context.read<UserCubit>().login(usernameController.text, passwordController.text);
+      body: Form(
+        key: formKey, // Lien avec la clé
+        child: Column(
+          children: [
+            TextFormField(
+              controller: usernameController, // Lien avec le controleur
+              validator: (value) {
+                // L'objectif du validator est de vérifier les données saisie
+                // La fonction doit renvoyée null si le champs est correcte,
+                // ou le message d'erreur à afficher si la valeur est mauvaise
+                if (value == null || value.isEmpty) {
+                  return 'Le champs doit être renseigné';
+                } else {
+                  return null;
+                }
               },
-              child: Text('Se connecter'))
-        ],
+            ),
+            TextFormField(
+              controller: passwordController, // Lien avec le controleur
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Le champs doit être renseigné';
+                } else {
+                  return null;
+                }
+              },
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  // "formKey.currentState" permet d'accéder au state du formulaire
+                  // et d'appeler la méthode validate
+
+                  // La méthode valider lance les "validators"
+                  // de tous les widgets de type "TextFormField"
+                  if (formKey.currentState!.validate()) {
+                    // Permet de récupérer la valeur du champ via le controleur
+                    final String username = usernameController.text;
+                    final String password = passwordController.text;
+                  }
+                },
+                child: const Text('Se connecter'))
+          ],
+        ),
       ),
     ));
   }
